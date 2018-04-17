2afe9cd282a95e3372a5670b9299525a24c9e17f
Mateusz Pytlak <mateuszpytlak@gmail.com>
2018-04-17 10:39:26 +0200

    zalozenie nowego repo

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require("path");
+module.exports={
+    entry: ['whatwg-fetch', './js/in.jsx'],
+    output: { filename: "out.js", path: path.resolve(__dirname, "js") },
+    mode: "development", watch: true,
+    module: {
+        rules: [{
+            test: /\.jsx$/, exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: { presets: ["es2015", "stage-2", "react"] }
+            }
+        }]
+    }
+}
