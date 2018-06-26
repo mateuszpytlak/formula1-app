bb7eb1709bab746435f8ebe23e6f6453dca9c5ea
Mateusz Pytlak <mateuszpytlak@gmail.com>
2018-06-26 20:01:54 +0200

    update

--- js/Buttons.jsx
+++ js/Buttons.jsx
@@ -7,6 +7,8 @@ export class Buttons extends React.Component {
             <div className="buttons">
                 {/*<NavLink to="/driverStandings" onClick={this.props.action} data-value="driverStandings">Drivers</NavLink>*/}
                 {/*<NavLink to="/constructorStandings" onClick={this.props.action} data-value="constructorStandings">Constructors</NavLink>*/}
+                {/*<NavLink to="/driverStandings" onClick={this.props.action} data-value="driverStandings">Drivers</NavLink>*/}
+                {/*<NavLink to="/constructorStandings" onClick={this.props.action} data-value="constructorStandings">Constructors</NavLink>*/}
                 <div onClick={this.props.action} data-value="driverStandings">Driver</div>
                 <div onClick={this.props.action} data-value="constructorStandings">Constructor</div>
                 {/* <div onClick={this.props.action} data-value="driverStandings">Driver</div>
